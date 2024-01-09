6cfb7dc68200a05cb535dd0f6c04298273fc667a
kkznch <kkznch710md@gmail.com>
2024-01-09 15:01:16 +0900

    pothosインストールした

--- packages/backend/src/schema.ts
+++ packages/backend/src/schema.ts
@@ -1,46 +1,13 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
-import { createSchema } from 'graphql-yoga';
-import { Context } from './context';
+import SchemaBuilder from '@pothos/core';
 
-const typeDefs = /* GraphQL */ `
-  scalar DateTime
+const builder = new SchemaBuilder({});
 
-  type User {
-    id: Int!
-    email: String!
-    name: String
-    writtenPosts: [Post]!
-    favoritedPosts: [Post]!
-    createdAt: DateTime
-    updatedAt: DateTime
-  }
-
-  type Post {
-    id: Int!
-    content: String
-    author: User
-    favoritedBy: [User]!
-    createdAt: DateTime
-    updatedAt: DateTime
-  }
-
-  type Query {
-    users: [User]!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    users: (_parent: any, _args: any, context: Context) => {
-      return context.prisma.user.findMany();
-    },
-  },
-};
-
-export const schema = createSchema<{
-  req: FastifyRequest;
-  reply: FastifyReply;
-}>({
-  typeDefs,
-  resolvers,
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      resolve: () => 'world',
+    }),
+  }),
 });
+
+export const schema = builder.toSchema();
