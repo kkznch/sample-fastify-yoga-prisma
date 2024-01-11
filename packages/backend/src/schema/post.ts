59b0d7f1864d0665449ae499c2cb17bcea591d0a
kkznch <kkznch710md@gmail.com>
2024-01-11 15:13:38 +0900

    mutation書いた

--- packages/backend/src/schema/post.ts
+++ packages/backend/src/schema/post.ts
@@ -1,4 +1,4 @@
-import { builder } from '../builder';
+import { builder, prisma } from '../builder';
 
 builder.prismaObject('Post', {
   name: 'Post',
@@ -7,5 +7,33 @@ builder.prismaObject('Post', {
     content: t.exposeString('content', { nullable: true }),
     author: t.relation('author'),
     favoritedBy: t.relation('favoritedBy'),
+  }),
+});
+
+const PostCreateInput = builder.inputType('PostCreateInput', {
+  fields: (t) => ({
+    content: t.string({ required: true }),
+  }),
+});
+
+builder.mutationType({
+  fields: (t) => ({
+    createPost: t.prismaField({
+      type: 'Post',
+      args: { input: t.arg({ type: PostCreateInput, required: true }) },
+      resolve: async (_query, _parent, args) => {
+        // This is just a demo, so we'll just grab the first user we find
+        const user = await prisma.user.findFirst();
+        if (!user) throw new Error('No user found');
+
+        const post = await prisma.post.create({
+          data: {
+            authorId: user.id,
+            content: args.input.content,
+          },
+        });
+        return post;
+      },
+    }),
   }),
 });
