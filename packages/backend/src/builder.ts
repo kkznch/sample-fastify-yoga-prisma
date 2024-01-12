adfe63d947b1ec9af9449666da51444d7912469d
kkznch <kkznch710md@gmail.com>
2024-01-12 17:15:39 +0900

    mutationのバリデーションできた

--- packages/backend/src/builder.ts
+++ packages/backend/src/builder.ts
@@ -2,16 +2,30 @@ import SchemaBuilder from '@pothos/core';
 import { PrismaClient, Prisma } from '@prisma/client';
 import PrismaPlugin from '@pothos/plugin-prisma';
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
+import WithInputPlugin from '@pothos/plugin-with-input';
+import ValidationPlugin from '@pothos/plugin-validation';
 
 export const prisma = new PrismaClient({});
 
 export const builder = new SchemaBuilder<{
   PrismaTypes: PrismaTypes;
 }>({
-  plugins: [PrismaPlugin],
+  plugins: [PrismaPlugin, WithInputPlugin, ValidationPlugin],
   prisma: {
     client: prisma,
     dmmf: Prisma.dmmf,
     filterConnectionTotalCount: true,
   },
+  withInput: {
+    argOptions: {
+      required: true,
+    },
+  },
+  validationOptions: {
+    validationError: (zodError, args, context, info) => {
+      return zodError;
+    },
+  },
 });
+
+builder.mutationType();
--- packages/backend/src/schema/post.ts
+++ packages/backend/src/schema/post.ts
@@ -1,4 +1,5 @@
 import { builder, prisma } from '../builder';
+import { createPostSchema } from '@libs/validator';
 
 builder.prismaObject('Post', {
   name: 'Post',
@@ -10,30 +11,27 @@ builder.prismaObject('Post', {
   }),
 });
 
-const PostCreateInput = builder.inputType('PostCreateInput', {
-  fields: (t) => ({
-    content: t.string({ required: true }),
-  }),
-});
+builder.mutationFields((t) => ({
+  createPost: t.prismaFieldWithInput({
+    type: 'Post',
+    input: {
+      content: t.input.string({ required: true }),
+    },
+    validate: {
+      schema: createPostSchema,
+    },
+    resolve: async (_query, _parent, args) => {
+      // This is just a demo, so we'll just grab the first user we find
+      const user = await prisma.user.findFirst();
+      if (!user) throw new Error('No user found');
 
-builder.mutationType({
-  fields: (t) => ({
-    createPost: t.prismaField({
-      type: 'Post',
-      args: { input: t.arg({ type: PostCreateInput, required: true }) },
-      resolve: async (_query, _parent, args) => {
-        // This is just a demo, so we'll just grab the first user we find
-        const user = await prisma.user.findFirst();
-        if (!user) throw new Error('No user found');
-
-        const post = await prisma.post.create({
-          data: {
-            authorId: user.id,
-            content: args.input.content,
-          },
-        });
-        return post;
-      },
-    }),
+      const post = await prisma.post.create({
+        data: {
+          authorId: user.id,
+          content: args.input.content,
+        },
+      });
+      return post;
+    },
   }),
-});
+}));
